ff3b0956e55c42ae7e14a78668eb0fdde56279dc
svensglinz <svenglinz@gmail.com>
2022-10-01 01:11:50 +0200

    push

--- margin_calculator.R
+++ margin_calculator.R
@@ -2,31 +2,64 @@ library(tidyverse)
 library(readxl)
 library(lubridate)
 library(zoo)
+library(runner)
 
+dat <- read_xlsx("Data/selected_futures_returns.xlsx")
+dat <- dat |> filter(INSTRUMENT == "FGBX")
 
-returns <- read_xlsx("Data/selected_futures_returns.xlsx")
+returns$DATE <- as.Date(returns$DATE)
+dates <-  returns |>
+  filter(INSTRUMENT == "FGBX") |>
+  select(DATE) |>
+  unlist()
 
-FGBX <- returns |> filter(INSTRUMENT == "FGBX") |> select(LOG_RET_1D) |> unlist() |> row_na
+FGBX <- returns |>
+  filter(INSTRUMENT == "FGBX") |>
+  select(LOG_RET_1D) |>
+  unlist()
+
 attr(FGBX, "names") <- NULL
 
-## somehow returns must also have a date indicated to them such that we can calculate historical margins as well! --> 
-#can circumvent by building a function which always gives the latest 500 returns!
+#--------------------------------
+#Input:
+#lambda: Decay factor (0,1]
+#returns: numeric vector which contains a series of log returns
+#The function calculates the n_day rolling exponentially-weighted-volatility 
+#for each ordered return observation
+#--------------------------------
 
-#calculates exponentially weighted volatility for a given vector of returns
+vola <- function(lambda, returns, n_day){
 
+options(warn = -1)  
+weights <- (1-lambda)* ((lambda) ^c(0:(length(returns)-1)))
 
-vola <- function(lambda, returns){
-  
-  
+roll_vola <- rollapply(returns, n_day,
+                       FUN = function(x){
+                         sum(x^2 *weights), align = "left", fill = NA)}
+
+return(roll_vola)
+
 }
 
-calculate_margin <- function(date, returns, MPOR, factor, log = TRUE, quantile, absolute = FALSE, lambda){
+#--------------------------------
+#Input:
+#returns: numeric vector which contains a series of log returns
+#MPOR: The liquidation period (ie. Time period over which VAR is calculated)
+#factor: 
+#log_ret: 
+#quantile: The quantile needed to calculate the VAR
+#absolute: 
+#lambda: Decay factor (0,1]
+#The function calculates the Margin Requirement according to the formula specified in XXX with 
+#the provided parameter specifications. 
+#--------------------------------
+
+calculate_margin <- function(returns, MPOR, factor, log_ret = TRUE, quantile, absolute = FALSE, lambda,
+                             floor = FALSE){
   
   #------------------------
   #load required packages
   #------------------------
-  
-  
   
   tryCatch(expr = {
   require("tidyverse")
@@ -40,15 +73,8 @@ calculate_margin <- function(date, returns, MPOR, factor, log = TRUE, quantile, 
   #returns
   if(!is.numeric(returns)) {stop("make sure that returns are a numeric vector")}
   
-  returns <- ifelse(isTRUE(log), returns, log(returns))
-  
-  #date
-  if(!is.Date(date)){
-    
-    e <- stop("Make sure to input a correct date!")
-    tryCatch(as.Date(date), 
-             error = function(e) e)
-  }
+  #check here if prices or log returns are provided?
+  #returns <- ifelse(isTRUE(log_ret), returns, log(returns))
   
   #MPOR
   if(!is.numeric(MPOR)){
@@ -69,21 +95,49 @@ calculate_margin <- function(date, returns, MPOR, factor, log = TRUE, quantile, 
   #Calculate Margin given all inputs
   #----------------------------
   
+  rolling_vola <- vola(lambda = 0.94, returns = returns)
   MPOR_returns <- rollsum(returns, MPOR)
+  vola_adj_returns <- MPOR_returns / rolling_vola[-c(1:2)] * rolling_vola[3]
   
   #-2 or something like that here for values at the end?
-  buckets <- c(rep(1:MPOR, length.out = length(returns)-2))
+  buckets <- rep(1:MPOR, length.out = (length(returns)-2))
   
   combined <- tibble(MPOR_returns = MPOR_returns, 
                         buckets = buckets)
   
-  stored <- 
-    combined |> group_by(buckets) |> summarize(VAR = quantile(MPOR_returns, 0.025, na.rm = T))
+  bucket_VAR <- 
+    combined |>
+    group_by(buckets) |>
+    summarize(VAR = quantile(MPOR_returns, 0.025, na.rm = T))
   
   
-  VAR <- stored |> summarize(mean = mean(VAR))
+  VAR <- bucket_VAR |>
+    summarize(mean = mean(VAR))
 
   VAR_upscaled <- VAR * factor
   
-  return(VAR_upscaled)
+  return(VAR_upscaled[[1]])
+}
+
+#-------------------------------------
+#-------------------------------------
+#-------------------------------------
+
+calculate_margins(returns, dates, ...){
+  
+  margins <- rollapply(data = returns,
+                       width = n_day,
+                       FUN = function(x){
+                         calculate_margin(returns = x,
+                                          MPOR = MPOR,
+                                          factor = factor, log_ret = TRUE,
+                                          quantile = quantile,
+                                          absolute = FALSE,
+                                          lambda = lambda,
+                                          floor = FALSE))}
+  
+  out <- tibble(dates = dates, 
+                returns = margins)
+  
+  return(out)
 }
