96455ce82b8d0b2dd48a36592168adf54346f547
svensglinz <svenglinz@gmail.com>
2022-11-19 20:46:06 +0100

    changes

--- functions_redone.R
+++ functions_redone.R
@@ -321,43 +321,48 @@ procyclicality_measures <- function(margins, start, end){
 #returns some goodness Margins defined in the paper by which we measure a margin model 
 #How is this connected to the default fund thing? 
 
-summary_stats <- function(margin_df, start, end){
+goodness_criteria <- function(margin_df, direction, start, end){
   
   start = as.Date(start, format = "%d/%m/%Y")
   end = as.Date(end, format = "%d/%m/%Y")
   
   margin_df <- margin_df |> 
-    filter(between(DATE, start, end)) |> 
-    mutate(MPOR_returns = exp(MPOR_returns)-1)
+    filter(between(DATE, start, end))
+  
+  if (direction == "long"){
+    margin_df$Margin <- margin_df$Margin *-1 
+  }
   
   N_observations <- length(na.omit(margin_df$MPOR_returns))
-  N_breaches <- sum(margin_df$Margin < margin_df$MPOR_returns*-1, na.rm = T)
-
+  N_breaches <- sum(margin_df$Margin > margin_df$MPOR_returns, na.rm = T) #account for NA= T (check)
+  #below when calculating percentage
   perc_breaches <- (N_breaches / N_observations)*100
+  
   realized_conf_level <- 100-perc_breaches
   
-  #gives average shortfall in % not in absolute terms!!! (must check!!!)
-  avg_shortfall <- margin_df |> filter(Margin < MPOR_returns*-1) |> 
-    mutate(shortfall = MPOR_returns*-1 - Margin) |> 
+  #gives average shortfall in % not in absolute terms!
+  avg_shortfall <- margin_df |> filter(Margin > MPOR_returns) |> 
+    mutate(shortfall = MPOR_returns - Margin) |> 
     summarize(avg_shortfall = mean(shortfall)) |>
     pull(avg_shortfall)
   
-  max_shortfall <- margin_df |> filter(Margin < MPOR_returns*-1) |> 
+  #also calculate maximum historical shortfall ? --> More appropriate for 
+  #default fund calculation???
+  #gives average shortfall in % not in absolute terms!
+  max_shortfall <- margin_df |> filter(Margin > MPOR_returns) |> 
     mutate(shortfall = MPOR_returns - Margin) |> 
     summarize(max_shortfall = min(shortfall)) |> 
     pull(max_shortfall)
   
+    #should we divide this by some kind of baseline scenario ? in what 
+ #measurement should we report this??? --> rapid and strong changes in day to day requirements 
+  #should add a multiplier to costs!!! --> Since stress is also cost and also additional financing costs!!!!!!
   costs <- mean(margin_df$Margin, na.rm = T)
   
-  peak_to_through <- max(margin_df$Margin, na.rm = T)/min(margin_df$Margin, na.rm = T)
-  max_1d <- max(diff(margin_df$Margin, lag = 1) / margin_df$Margin[-length(margin_df$Margin)], na.rm = T)
-  max_5d <- max(diff(margin_df$Margin, lag = 5) / margin_df$Margin[-c(length(margin_df$Margin):(length(margin_df$Margin)-4))], na.rm = T)
-  max_30d <- max(diff(margin_df$Margin, lag = 20) / margin_df$Margin[-c(length(margin_df$Margin):(length(margin_df$Margin)-19))], na.rm = T) #30days = 20 business days!
-  
   out <- tibble(type = c("N_breaches", "N_observations", "perc_breaches", "conf_level", "avg_shortfall",
-                  "max_shortfall", "costs", "peak_to_through", "max_1d", "max_5d", "max_30d"),
+                  "max_shortfall"),
                 values = c(N_breaches, N_observations, perc_breaches, realized_conf_level,
-                           avg_shortfall, max_shortfall, costs, peak_to_through, max_1d, max_5d, max_30d))
+                           avg_shortfall, max_shortfall))
   
   return(out)
 }
